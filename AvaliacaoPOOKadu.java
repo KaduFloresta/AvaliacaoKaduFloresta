0f3149debb026db994c61af78cf3dfa414e482c2
CARLOS EDUARDO FLORESTA <carlos.floresta@alunos.sc.senac.br>
2019-10-07 20:07:24 -0300

    Prova Senac ADS POO

--- AvaliacaoPOOKadu.java
+++ AvaliacaoPOOKadu.java
@@ -1,47 +1,78 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class AvaliacaoPOOKadu {
     public static void main(String[] args) {
-    Scanner leituratxt = new Scanner(System.in);
-    System.out.println("Kadu Floresta");
-    
-    /*
-    O programa deverá receber uma lista de valores de um arquivo de texto,
-    onde esta lista não terá uma determinação de quantidade fixa,
-    podendo variar de arquivo para arquivo. Após, deverá ser alocado os
-    valores dentro da estrutura de Array (não poderão ser aplicadas Collections
-    para solução), e realizada a ordenção dos valores, imprimindo em tela os passos
-    ocorridos desta ordenação. Após, deverá ser utilizado uma Collection para o
-    recebimento dos mesmos valores, de maneira que estes valores já estejam ordenados.
-    Ao final, deverá ser impresso em tela o valor ordenado do Array e da Collection,
-    garantindo que estejam iguais, e deverá ser exportado em um arquivo de texto 
-    esses valores.
-    */ 
-
-    System.out.printf("\nConteúdo do Arquivo Texto:\n");
+        System.out.println("Kadu Floresta - Avaliação POO Java");
+        System.out.printf("\nConteúdo do Arquivo Texto:\n");
+        
         try {
-            FileReader arq = new FileReader("avaliacao.txt");
+            // Lendo os dados do TXT
+            FileReader arq = new FileReader("avaliacaoimport.txt");
             BufferedReader lerArq = new BufferedReader(arq);
-    
-            /*
-            lê 1º linha a variável "linha" recebe o valor "null" quando o
-            processo de repetição atingir o final do arquivo texto
-            */
-            String str; 
-                while ((str = lerArq.readLine()) !=null) {
-                System.out.printf(str + "\n");
-            
+                       
+            // String recebendo valores do TXT.
+            String[] arrayA = lerArq.readLine().split(",");
+            System.out.println(Arrays.toString(arrayA));
+            int[] solucaoA = new int[arrayA.length]; 
+          
+            // String em Int
+            for (int i = 0; i < arrayA.length; i++) {
+                solucaoA[i] = Integer.parseInt(arrayA[i]);
             }
-        lerArq.close();
+            // Encerrando o Buffer de leitura do TXT
+            lerArq.close();
+
+            // Insertion Sort
+            System.out.println("\nProcesso de Ordenação: ");
+            for (int i = 0; i < solucaoA.length; i++) {
+                int atual = solucaoA[i];
+                int j = i - 1;
+                while (j >= 0 && solucaoA[j] >= atual) {
+                    solucaoA[j + 1] = solucaoA[j];
+                    j--;
+                }
+                solucaoA[j + 1] = atual;
+
+            // Impressão do Processo de Ordenação.
+            System.out.println(Arrays.toString(solucaoA));
+            }
+            // Collection 
+            ArrayList<Integer> listA = new ArrayList<>();
+            // Lista da Collection
+            for (int i = 0; i < solucaoA.length; i++) {
+                listA.add(solucaoA[i]);
+            }
+            // Exportando os dados em TXT
+            FileWriter arq2 = new FileWriter("avaliacaoexport.txt");
+            BufferedWriter expArq = new BufferedWriter(arq2);
+
+            // Impressão do Array Ordenado
+            System.out.println("\nARRAY: ");
+            for (int i = 0; i < solucaoA.length; i++) {
+                System.out.println(solucaoA[i]);
+                //
+                expArq.append(" " + solucaoA[i]);
+            }
+            // Impressão da Collection Ordenada
+            System.out.println("\nCOLLECTION: ");
+            for (int numA:listA){
+                System.out.println(numA);
+            }             
+            //Espaço
+            System.out.println("\n");
+
+            // Encerrando o Buffer de exportação do TXT
+            expArq.close();
+
+            // Exceção caso não leia o TXT
+        } catch (IOException e) {
+            System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
         }
-            catch (IOException e) {
-            System.err.printf("Erro na abertura do arquivo: %s.\n",
-            e.getMessage());
-        }
-    leituratxt.close();
-    System.out.println();
     }
 }
